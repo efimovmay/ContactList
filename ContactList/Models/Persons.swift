9ced68dfd62c82c85aa864b910e86b0d6e3c6bce
Aleksey Efimov <smaylik121@gmail.com>
2023-01-23 22:29:09 +0300

    Add DataStorage

--- ContactList/Models/Persons.swift
+++ ContactList/Models/Persons.swift
@@ -5,42 +5,34 @@
 //  Created by Aleksey on 23.01.2023.
 //
 
-struct Persons {
-    let name: [String]
-    let surename: [String]
-    let phone: [String]
-    let email: [String]
+struct Person {
+    let name: String
+    let sureName: String
+    let phone: String
+    let email: String
     
-    var fullName: [String] {
-        name + surename
+    static func getPersons() -> [Person] {
+        var persons: [Person] = []
+        
+        let names = DataStore.name.shuffled()
+        let sureNames = DataStore.sureName.shuffled()
+        let phones = DataStore.phone.shuffled()
+        let emails = DataStore.email.shuffled()
+        
+        for number in 0..<names.count {
+            let person = Person(name: names[number],
+                                sureName: sureNames[number],
+                                phone: phones[number],
+                                email: emails[number])
+            persons.append(person)
+        }
+        return persons
     }
+    
+//    var fullName: String {
+//        name + surename
+//    }
 }
 
 
-class DataStore {
-    let name = ["Erick",
-                "Peter",
-                "Mary",
-                "Elfrieda",
-                "Edwina",
-                "Scott",
-                "Myron",
-                "Jeremy",
-                "John",
-                "Patricia"]
-    
-    let surename = ["Briggs",
-                    "Palmer",
-                    "Griffith",
-                    "Craig",
-                    "Hill",
-                    "McKenzie",
-                    "Richardson",
-                    "Bond", "Harris",
-                    "Stewart"]
-    
-    let phone = ["Erick", "", "", "", "", "", "", "", "", ""]
-    let email = ["Erick", "", "", "", "", "", "", "", "", ""]
-    
-    
-}
+
